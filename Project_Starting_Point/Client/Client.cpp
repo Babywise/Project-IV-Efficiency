888c79025de15ae2184d572423de057c3c8108aa
nickvprince <nickvprince@gmail.com>
2023-01-17 12:51:49 -0500

    Commented Client

--- Project_Starting_Point/Client/Client.cpp
+++ Project_Starting_Point/Client/Client.cpp
@@ -13,7 +13,7 @@ int main()
 	WSADATA wsaData;
 	SOCKET ClientSocket;
 	sockaddr_in SvrAddr;
-	unsigned int uiSize = 0;
+	unsigned int uiSize = 0; // uiSize = unsignedInt size?
 	vector<string> ParamNames;
 	char Rx[128];
 
@@ -24,7 +24,9 @@ int main()
 	SvrAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
 	connect(ClientSocket, (struct sockaddr*)&SvrAddr, sizeof(SvrAddr));
 
-	uiSize = GetSize();
+	uiSize = GetSize(); 
+
+	// Loop through number of lines in the file
 	for (unsigned int l = 0; l < uiSize; l++)
 	{
 		string strInput;
@@ -35,23 +37,27 @@ int main()
 		getline(ifs, strInput);
 		if (l > 0)
 		{
-			size_t offset, preOffset;
+			size_t offset, preOffset; // Keeps track of which value to read (param position)
 			offset = preOffset = 0;
 			unsigned int iParamIndex = 0;
 			//while (offset != std::string::npos)
+
+			// This loop gets the parameter names and values. These are sent to the server where we recieve an acknowledgement
 			while(iParamIndex != 8)
 			{
-				offset = strInput.find_first_of(',', preOffset+1);
-				string strTx = strInput.substr(preOffset+1, offset - (preOffset+1));
-				send(ClientSocket, ParamNames[iParamIndex].c_str(), (int)ParamNames[iParamIndex].length(), 0);
-				recv(ClientSocket, Rx, sizeof(Rx), 0);
-				send(ClientSocket, strTx.c_str(), (int)strTx.length(), 0);
-				recv(ClientSocket, Rx, sizeof(Rx), 0);
-				cout << ParamNames[iParamIndex] << " Avg: " << Rx << endl;
-				preOffset = offset;
-				iParamIndex++;
+				offset = strInput.find_first_of(',', preOffset+1); // Find comma, get size of everything after it
+				// Creates a substring for the param name. Uses the offset values to know where to start and end
+				string strTx = strInput.substr(preOffset+1, offset - (preOffset+1)); 
+				send(ClientSocket, ParamNames[iParamIndex].c_str(), (int)ParamNames[iParamIndex].length(), 0); // Send parameter name to server
+				recv(ClientSocket, Rx, sizeof(Rx), 0); // Recieve Ack
+				send(ClientSocket, strTx.c_str(), (int)strTx.length(), 0); // Send value to server
+				recv(ClientSocket, Rx, sizeof(Rx), 0); // Recieve Average
+				cout << ParamNames[iParamIndex] << " Avg: " << Rx << endl; // Print param name and average
+				preOffset = offset; // Update offset to next column
+				iParamIndex++; // Increment index of param to read from buffer
 			}
 		}
+		// When l is 0 (first iteration of loop)
 		else
 		{
 			ParamNames.push_back("TIME STAMP");
@@ -75,6 +81,7 @@ int main()
 	return 1;
 }
 
+/// <returns>uiSize (number of lines in the file)</returns>
 unsigned int GetSize()
 {
 	string strInput;

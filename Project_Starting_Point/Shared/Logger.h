370752ccf27ac4371db239590ded8336257daa75
Danny Smith <Dsmith2304@conestogac.on.ca>
2023-01-24 11:57:39 -0500

    Logger files added from https://dev.azure.com/Ecoleshill/CSCN73030-F22/_git/Quizzes
    Filters included for shared files in server and client projects.
    
    Log folder added and shared folder added to root project directory

---
+++ Project_Starting_Point/Shared/Logger.h
@@ -0,0 +1,8 @@
+#pragma once
+#include <string>
+class Logger {
+public:
+	void static log(std::string message);
+	void static log(std::string message, int severity);
+	void static log(std::string message,int severity,std::string logname);
+};

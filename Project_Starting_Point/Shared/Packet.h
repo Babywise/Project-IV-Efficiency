ad53d9e5e2122c1c14ddb7df6fd8f85950435df0
Babywise <islamdoesart@gmail.com>
2023-03-02 14:50:31 -0500

    comments added to Packet.h

--- Project_Starting_Point/Shared/Packet.h
+++ Project_Starting_Point/Shared/Packet.h
@@ -1,10 +1,14 @@
 #pragma once
 #include <string>
 
+//Constants for packet structure
 const int maxParamNameStringLength = 64;
 const int maxIPStringLength = 16;
 const int maxTimeStampStringLength = 6;
 
+/// <summary>
+/// Packet Structure for sending FlightData
+/// </summary>
 class Packet {
 
 	struct Header {
@@ -23,12 +27,24 @@ class Packet {
 	char* TxBuffer = {};
 
 public:
+	/// <summary>
+	/// Deserializing Constructor
+	/// </summary>
+	/// <param name="RxBuffer"></param>
 	Packet(char* RxBuffer)
 	{
 		std::memcpy(&this->head, RxBuffer, sizeof(this->head));
 		std::memcpy(&this->FD, RxBuffer + sizeof(this->head), sizeof(this->FD));
 	}
-
+	/// <summary>
+	/// Packet Creation Constructor
+	/// </summary>
+	/// <param name="sourceIP"></param>
+	/// <param name="destinationIP"></param>
+	/// <param name="paramName"></param>
+	/// <param name="planeID"></param>
+	/// <param name="timestamp"></param>
+	/// <param name="fuelTotalQuantity"></param>
 	Packet(std::string sourceIP, std::string destinationIP, std::string paramName, int planeID, std::string timestamp, float fuelTotalQuantity)
 	{
 		std::memcpy(&this->head.srcIP, sourceIP.c_str(), sourceIP.length());
@@ -39,7 +55,10 @@ public:
 		std::memcpy(&this->FD.paramName, paramName.c_str(), paramName.length());
 		this->FD.fuelTotalQuantity = fuelTotalQuantity;
 	}
-
+	/// <summary>
+	/// Serialize function
+	/// </summary>
+	/// <returns>char* buffer</returns>
 	char* serialize()
 	{
 		this->head.seq++;
@@ -51,22 +70,33 @@ public:
 
 		return this->TxBuffer;
 	}
-
+	/// <summary>
+	/// Increase Packet Sequence Number by 1
+	/// </summary>
 	void incrementSeq()
 	{
 		this->head.seq++;
 	}
-
+	/// <summary>
+	/// Retrieve the current Sequence Number
+	/// </summary>
+	/// <returns>int SequenceNumber</returns>
 	int getSeq() {
 		return this->head.seq;
 	}
-
+	/// <summary>
+	/// Retrieve the Timestamp from the FlightData
+	/// </summary>
+	/// <returns>std::string timestamp</returns>
 	std::string getTimestamp()
 	{
 		std::string timestamp = this->FD.timestamp;
 		return timestamp;
 	}
-
+	/// <summary>
+	/// Retrieve the FuelTotalQuantity from the FlightData
+	/// </summary>
+	/// <returns>float FuelTotalQuantity</returns>
 	float getFuelTotalQuantity()
 	{
 		return this->FD.fuelTotalQuantity;
